ce59e574724126511c6aab36507dc8f196b82737
thomasalvatran <thomasalvatran@gmail.com>
2014-09-26 16:14:31 -0400

    usb driver

---
+++ stick_driver.mod.c
@@ -0,0 +1,35 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+ .name = KBUILD_MODNAME,
+ .init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+ .exit = cleanup_module,
+#endif
+ .arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0xf6628fc9, "module_layout" },
+	{ 0x3ac8c215, "usb_deregister" },
+	{ 0x63afefa5, "usb_register_driver" },
+	{ 0x27e1a049, "printk" },
+	{ 0xb4390f9a, "mcount" },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+MODULE_ALIAS("usb:v054Cp0439d*dc*dsc*dp*ic*isc*ip*");
+MODULE_ALIAS("usb:v14CDp125Ad*dc*dsc*dp*ic*isc*ip*");
+
+MODULE_INFO(srcversion, "5DE9052E4ED9A14ACE010B1");
